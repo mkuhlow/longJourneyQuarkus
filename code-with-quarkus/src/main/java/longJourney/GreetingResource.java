22dc8fc77596ae1aebfe2c9654973ad5a24441d4
MarcelKuhlow <Marcel.Kuhlow@codefrog.de>
2021-07-09 07:09:26 +0200

    add service - using Injection

--- code-with-quarkus/src/main/java/longJourney/GreetingResource.java
+++ code-with-quarkus/src/main/java/longJourney/GreetingResource.java
@@ -1,16 +1,28 @@
 package longJourney;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
-    }
+	@Inject
+	GreetingService greetingService;
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/greeting/{name}")
+	public String greeting(@PathParam String name) {
+		return greetingService.greeting(name);
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "Hello RESTEasy";
+	}
 }
---
+++ code-with-quarkus/src/main/java/longJourney/GreetingService.java
@@ -0,0 +1,11 @@
+package longJourney;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    public String greeting(String name) {
+        return "Hello " + name;
+    }
+}
