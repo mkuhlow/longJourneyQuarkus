439e378afc8cbe406417a66fc61c5ce6051e9fd9
MarcelKuhlow <Marcel.Kuhlow@codefrog.de>
2021-07-09 14:13:48 +0200

    refactoring Tests

--- code-with-quarkus/src/test/java/longJourney/GreetingResourceGqlTest.java
+++ code-with-quarkus/src/test/java/longJourney/GreetingResourceGqlTest.java
@@ -1,8 +1,6 @@
 package longJourney;
 
 import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.RestAssured;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -25,31 +23,6 @@ public class GreetingResourceGqlTest {
         Assertions.assertEquals("Hello RESTEasy", greetingResource.hello());
     }
 
-
-    //String query = "{Greeting}";
-
-    /*
-    @Test
-    public void testQuery2() {
-        RestAssured.given()
-                .when()
-                .post("/graphql")
-                .then()
-                .statusCode(200)
-                .body("data", Matchers.notNullValue());
-    }
-
-    @Test
-    public void testQuery() {
-        RestAssured.given()
-                .when()
-                .contentType("application/json")
-                .body(query)
-                .post("/graphql")
-                .then()
-                .statusCode(200)
-                .body("data", Matchers.notNullValue());
-    }*/
     }
 
 
